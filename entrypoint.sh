44d03c370337423dbda8905a0f6bce2d5e245b3a
Konstantinos Dichalas <kdihalas@gmail.com>
2020-01-19 23:40:29 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,4 @@ fi
 
 
 
-/pack/pack build $IMAGE_NAME -p /github/workspace/$SOURCE_LOCATION --builder $BUILDER $PUBLISH_FLAG
+/pack/pack build $IMAGE_NAME -p /github/workspace/$SOURCE_LOCATION/$CONTEXT --builder $BUILDER $PUBLISH_FLAG
