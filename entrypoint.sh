0bab5808d273f27ed3fe294dd430f73506ae639f
Konstantinos Dichalas <kdihalas@gmail.com>
2020-01-19 23:55:04 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,9 @@
-
-
 if [ -n "$PUBLISH" ]; then
   PUBLISH_FLAG="--publish"
 fi
 
+if [ -z "$VARS" ]; then
+  ENV_FLAG="$VARS"
+fi
 
-
-/pack/pack build $IMAGE_NAME -p /github/workspace/$SOURCE_LOCATION/$CONTEXT --builder $BUILDER $PUBLISH_FLAG
+/pack/pack build $IMAGE_NAME -q -p /github/workspace/$SOURCE_LOCATION/$CONTEXT --builder $BUILDER $PUBLISH_FLAG $ENV_FLAG
